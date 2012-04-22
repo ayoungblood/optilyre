a47aab406060a5a0a92efbff5380014ab12d249f
Akira Youngblood <akirayoungblood@me.com>
2012-04-22 09:37:27 -0600

    added constant interface elements in opti0.js Control interface

---
+++ opti0.js
@@ -0,0 +1,57 @@
+loadedInterfaceName = "optilyre 0";
+interfaceOrientation = "portrait";
+
+constants = [
+{
+    "name":"page",
+    "type":"Button",
+    "isLocal":true,
+    "label":"PAGE",
+    "x":.015, "y":.01,
+    "width":.23125, "height":.09,
+    "color":"#003040",
+    "stroke":"#00bfff",
+    "backgroundColor":"#003040",
+    // "ontouchstart":"bloat.changePage()",
+},
+{
+    "name":"menu",
+    "type":"Button",
+    "x":.26125, "y":.01,
+    "width":.23125, "height":.09,
+    "isLocal":true,
+    "label":"MENU",
+    "mode":"toggle",
+    "color":"#0000bf",
+    "stroke":"#00bfff",
+    "backgroundColor":"#003040",
+    "ontouchstart":"if(this.value == this.max) {control.showToolbar();} else{control.hideToolbar();}",
+},
+{
+    "name":"refresh",
+    "type":"Button",
+    "isLocal":true,
+    "label":"FRESH",
+    "x":.5075, "y":.01,
+    "width":.23125, "height":.09,
+    "color":"#0000bf",
+    "stroke":"#00bfff",
+    "backgroundColor":"#003040",
+    "mode":"toggle",
+    "ontouchstart":"interfaceManager.refreshInterface()",
+},
+{
+    "name":"info",
+    "type":"Button",
+    "isLocal":true,
+    "label":"INFO",
+    "x":.75375, "y":.01,
+    "width":.23125, "height":.09,
+    "color":"#03040",
+    "stroke":"#00bfff",
+    "backgroundColor":"#003040",
+    "mode":"momentary",
+    "ontouchstart":"control.changePage(3)",
+},
+
+]; // end constants
