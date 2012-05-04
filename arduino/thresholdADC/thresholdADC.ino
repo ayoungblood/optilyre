91a6947db7cc0e2d04993a8667cc26f8265fc3f2
Akira Youngblood <akirayoungblood@me.com>
2012-05-03 19:23:50 -0600

    Beginning of the Arduino side of the optilyre
    Arduino client for optilyre. 2012 May 03 Akira Youngblood Check analog
    pins, when value is greater than threshold, trigger and inform
    Processing sketch via serial. Threshold must be able to be set via the
    Processing sketch.

---
+++ arduino/thresholdADC/thresholdADC.ino
@@ -0,0 +1,12 @@
+/* Arduino client for optilyre. 2012 May 03 Akira Youngblood
+ * Check analog pins, when value is greater than threshold,
+ * trigger and inform Processing sketch via serial.
+ */
+ 
+void setup() {
+  Serial.begin(9600); // the bitrate must match the Processing sketch.
+  
+}
+void loop() {
+  
+}
