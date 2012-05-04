b697c77ddebd17c1063c9d3867a4bd22673e6c5d
Akira Youngblood <akirayoungblood@me.com>
2012-05-03 19:31:09 -0600

    Using array for analog pins, iterating through and checking against threshold

--- arduino/thresholdADC/thresholdADC.ino
+++ arduino/thresholdADC/thresholdADC.ino
@@ -1,12 +1,23 @@
 /* Arduino client for optilyre. 2012 May 03 Akira Youngblood
  * Check analog pins, when value is greater than threshold,
  * trigger and inform Processing sketch via serial.
+ * The threshold must be able to be set via the Processing sketch.
+ * Note that it takes 100 microseconds to read an ADC, so max frequency is 10kHz
  */
- 
+
+int threshold = 0;
+int inputs[] = {A0,A1,A2,A3,A4,A5,A6};
+
 void setup() {
   Serial.begin(9600); // the bitrate must match the Processing sketch.
   
 }
 void loop() {
-  
+  for (int i=0; i<6; i++) {
+    if (analogRead(inputs[i]) > threshold) {
+      
+      // foo
+      
+    }
+  }
 }
